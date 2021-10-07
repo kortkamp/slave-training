8a3d25fe81d809fb3a5f70a09a777b69c150482d
kortkamp <marcelusmedius@gmail.com>
2021-10-07 20:15:54 -0300

    feat: use Slaveprovider hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { SlaveProvider } from './hooks/useSlave';
+
+import { GlobalStyle } from './styles/global';
 
 import Routes from './routes';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes />
+      <SlaveProvider>
+        <Routes />
+      </SlaveProvider>
+      <GlobalStyle />
     </BrowserRouter>
   );
 }
