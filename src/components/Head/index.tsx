85382312fc153d12418c26fb2ab0146e729aec8b
kortkamp <marcelusmedius@gmail.com>
2021-10-07 20:14:35 -0300

    feat: create Head

---
+++ src/components/Head/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from './styles';
+
+import headImg from '../../assets/fgimage/Hx1/body/head0.png';
+
+const Head = ():JSX.Element => (
+  <Container>
+    <img src={headImg} alt="" />
+  </Container>
+);
+
+export default Head;
