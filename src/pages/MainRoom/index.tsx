ff3bac81f7e6ff9ad98aa15694ec8c689a9ebef7
kortkamp <marcelusmedius@gmail.com>
2021-10-07 20:15:15 -0300

    feat: create MainRoom

--- src/pages/MainRoom/index.tsx
+++ src/pages/MainRoom/index.tsx
@@ -1,9 +1,15 @@
 import { Container } from './styles';
+import Body from '../../components/Body';
+
+import backgroundImg from '../../assets/bgimage/H/Hx1_0.jpg';
+
+import testImg from '../../assets/fgimage/Hx1/tools/plug.png';
 
 const MainRoom = ():JSX.Element => (
   <Container>
-    <p>main room</p>
+    <img src={backgroundImg} alt="" />
+    <Body />
+    <img src={testImg} alt="" />
   </Container>
 );
-
 export default MainRoom;
--- src/pages/MainRoom/styles.ts
+++ src/pages/MainRoom/styles.ts
@@ -1,7 +1,20 @@
-/* eslint-disable import/prefer-default-export */
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  transform:scale(0.8);
+  width: 0px;
+  height:0px;
+  position: relative;
+  img {
+    top:0;
+    left:0;
+    position:absolute;
+    
+  }
+`;
 
-
+export const Bed = styled.img`
+  
+  
+  
 `;
