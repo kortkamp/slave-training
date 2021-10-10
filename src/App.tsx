5b9488b54596627198485ca2685d863afba6a4be
kortkamp <marcelusmedius@gmail.com>
2021-10-10 11:10:11 -0300

    refactor: change scale to global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { SlaveProvider } from './hooks/useSlave';
 
+import { SlaveProvider } from './hooks/useSlave';
+import { useWindowDimensions } from './hooks/useWindowDimensions';
 import { GlobalStyle } from './styles/global';
 
 import Routes from './routes';
 
 function App() {
+  const { scale } = useWindowDimensions();
   return (
     <BrowserRouter>
       <SlaveProvider>
         <Routes />
       </SlaveProvider>
-      <GlobalStyle />
+      <GlobalStyle scale={scale} />
     </BrowserRouter>
   );
 }
