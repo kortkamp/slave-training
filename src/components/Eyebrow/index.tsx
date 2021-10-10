43659fd1f02c48234cbdcf23b5078a206fdf51f7
kortkamp <marcelusmedius@gmail.com>
2021-10-10 11:06:29 -0300

    feat: add eyebrow

---
+++ src/components/Eyebrow/index.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+import { Container } from './styles';
+
+import eyebrowDefault from '../../assets/fgimage/Hx1/body/y_def.png';
+import eyebrowConform from '../../assets/fgimage/Hx1/body/y_conf.png';
+import eyebrowPain from '../../assets/fgimage/Hx1/body/y_conf_h.png';
+import { useEmotion } from '../../hooks/useEmotion';
+
+const images = [
+  eyebrowDefault,
+  eyebrowConform,
+  eyebrowPain,
+];
+
+const Eyebrow = ():JSX.Element => {
+  const { expression } = useEmotion();
+  return (
+    <Container>
+      <img src={images[expression.head.eyebrow]} alt="" />
+    </Container>
+  );
+};
+
+export default Eyebrow;
