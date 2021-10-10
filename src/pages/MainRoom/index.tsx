7f8d6adc065e9d4acac7426bf785ff31b8bfb7f8
kortkamp <marcelusmedius@gmail.com>
2021-10-10 11:08:58 -0300

    refactor: clear component

--- src/pages/MainRoom/index.tsx
+++ src/pages/MainRoom/index.tsx
@@ -1,15 +1,38 @@
 import { Container } from './styles';
 import Body from '../../components/Body';
 
+// eslint-disable-next-line no-unused-vars
+import { useMousePosition } from '../../hooks/useMousePosition';
+
+import StatusBox from '../../components/StatusBox';
+
 import backgroundImg from '../../assets/bgimage/H/Hx1_0.jpg';
+// import { useSlave } from '../../hooks/useSlave';
 
-import testImg from '../../assets/fgimage/Hx1/tools/plug.png';
+// import testImg from '../../assets/fgimage/Hx1/tools/plug.png';
 
-const MainRoom = ():JSX.Element => (
-  <Container>
-    <img src={backgroundImg} alt="" />
-    <Body />
-    <img src={testImg} alt="" />
-  </Container>
-);
+const MainRoom = ():JSX.Element => {
+  // eslint-disable-next-line no-unused-vars
+  // const position = useMousePosition();
+  console.log('main room redraw');
+  // const { status } = useSlave();
+  return (
+    <Container>
+
+      <img className="background" src={backgroundImg} alt="" />
+
+      <Body />
+      {/* <img src={testImg} alt="" /> */}
+      {/* <div>
+        {position.x}
+        :
+        {position.y}
+        --
+        {' '}
+        {status.lust}
+      </div> */}
+      <StatusBox />
+    </Container>
+  );
+};
 export default MainRoom;
