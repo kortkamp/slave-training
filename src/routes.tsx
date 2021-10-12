cedbaf333621474081305b1507cd06be80a52872
kortkamp <marcelusmedius@gmail.com>
2021-10-11 22:16:13 -0300

    feat: add new pages

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,14 @@
 import { Switch, Route } from 'react-router-dom';
 
-import MainRoom from './pages/MainRoom';
+import TrainingBed from './pages/TrainingBed';
+import Bedroom from './pages/Bedroom';
+import RestRoom from './pages/RestRoom';
 
 const Routes = ():JSX.Element => (
   <Switch>
-    <Route path="/" exact component={MainRoom} />
+    <Route path="/" exact component={TrainingBed} />
+    <Route path="/bedroom" exact component={Bedroom} />
+    <Route path="/RestRoom" exact component={RestRoom} />
   </Switch>
 );
 
