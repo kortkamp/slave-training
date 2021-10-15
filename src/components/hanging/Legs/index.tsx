35c4739f99faac986489a26d7103ea1fe77c8157
kortkamp <marcelusmedius@gmail.com>
2021-10-15 14:49:19 -0300

    feat: add legs animation

--- src/components/hanging/Legs/index.tsx
+++ src/components/hanging/Legs/index.tsx
@@ -1,9 +1,12 @@
 import { Container } from './styles';
+// import { useEmotion } from '../../../hooks/useEmotion';
 
 import legsDown from '../../../assets/fgimage/hang/hang_legs_down.png';
 import legsMiddle from '../../../assets/fgimage/hang/hang_legs_middle.png';
 import legsUp from '../../../assets/fgimage/hang/hang_legs_up.png';
-import { useEmotion } from '../../../hooks/useEmotion';
+import legLeft from '../../../assets/fgimage/hang/left_leg.png';
+import legRight from '../../../assets/fgimage/hang/right_leg.png';
+import { useSlave } from '../../../hooks/useSlave';
 
 const legsImages = [
   legsDown,
@@ -11,11 +14,33 @@ const legsImages = [
   legsUp,
 ];
 
-const Legs = ():JSX.Element => {
-  const { expression } = useEmotion();
+interface ILegsProps {
+  liftHeight: number;
+}
+
+const Legs = ({ liftHeight }:ILegsProps):JSX.Element => {
+  // const { expression } = useEmotion();
+  const { status } = useSlave();
+  // const force = 10 * Math.round((status.health / 10) / 10);
+  let swingingSpeed = '';
+  if (status.health > 50) {
+    swingingSpeed = 'swinging3';
+  } else if (status.health > 25) {
+    swingingSpeed = 'swinging2';
+  } else if (status.health > 0) {
+    swingingSpeed = 'swinging1';
+  }
+  const legPosition = Math.round((status.oxygen / 10) % 2);
   return (
     <Container>
-      <img src={legsImages[expression.face.legs || 0]} alt="" />
+      {status.oxygen <= 0 && liftHeight > 0
+        ? (
+          <>
+            <img className={`repositionedRightLeg ${swingingSpeed}`} src={legRight} alt="" />
+            <img className={`repositionedLeftLeg ${swingingSpeed}`} src={legLeft} alt="" />
+          </>
+        )
+        : <img src={legsImages[2 - legPosition]} alt="" />}
     </Container>
   );
 };
--- src/components/hanging/Legs/styles.ts
+++ src/components/hanging/Legs/styles.ts
@@ -1,5 +1,44 @@
 import styled from 'styled-components';
 
+const swingTime = 0.75;
+const swingAngle = 4;
+
 export const Container = styled.div`
 
+  .repositionedLeftLeg {
+    left: 580px;
+    top: 540px;
+    
+    transform-origin: 120px 70px;
+
+  }
+  .swinging3 {
+    animation: swingingLegs ${swingTime / 3}s ease-in-out infinite;
+  }
+  .swinging2 {
+    animation: swingingLegs ${swingTime / 2}s ease-in-out infinite;
+  }
+  .swinging1 {
+    animation: swingingLegs ${swingTime}s ease-in-out infinite;
+  }
+
+  .repositionedRightLeg {
+    left: 460px;
+    top: 520px;
+    
+    transform-origin: 170px 50px;
+    animation-delay: -${swingTime / 4}s;
+  }
+
+  
+
+
+  @keyframes swingingLegs {
+    0% , 100%  {
+      transform: rotate(${swingAngle}deg)  scaleY(0.9);
+    }
+    50% {
+      transform:  rotate(-${swingAngle}deg) scaleY(0.9);
+    }
+  }
 `;
