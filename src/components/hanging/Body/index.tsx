4931df1cc42de65d4844b5d95e7ea71630e6c8e8
kortkamp <marcelusmedius@gmail.com>
2021-10-15 14:52:20 -0300

    feat: improve body expressions

--- src/components/hanging/Body/index.tsx
+++ src/components/hanging/Body/index.tsx
@@ -1,20 +1,32 @@
-import { Container } from './styles';
+import { Container, LiftContainer } from './styles';
 
-import bodyImg from '../../../assets/fgimage/hang/hang_body.png';
+import bodyImg from '../../../assets/fgimage/hang/hang_body2.png';
 import Legs from '../Legs';
 import Head from '../Head';
 
+import ropeImg from '../../../assets/fgimage/hang/rope.png';
+
 interface IBodyState {
-  state: 'sit'|'lift'|'swing'
+  hands: 'tied'|'released';
+  neck: 'tied'|'released';
+  liftHeight: number;
 }
-const Body = ({ state }:IBodyState):JSX.Element => (
-  <Container hangAngle={1} className={state}>
 
-    <img src={bodyImg} alt="" />
-    <Legs />
-    <Head position={state === 'sit' ? 0 : 1} />
+const Body = ({
+  // eslint-disable-next-line no-unused-vars
+  hands, neck, liftHeight,
+}:IBodyState):JSX.Element => (
+  <LiftContainer liftHeight={liftHeight}>
+    <Container swingAngle={5} className={liftHeight > 0 ? 'swing' : 'sit'}>
 
-  </Container>
+      <img src={bodyImg} alt="" />
+      <Legs liftHeight={liftHeight} />
+      {neck === 'tied' && <img className="ropeImg" src={ropeImg} alt="" />}
+      <Head liftHeight={liftHeight} />
+
+    </Container>
+
+  </LiftContainer>
 );
 
 export default Body;
--- src/components/hanging/Body/styles.ts
+++ src/components/hanging/Body/styles.ts
@@ -1,39 +1,60 @@
 import styled from 'styled-components';
 
 interface IBodyContainer {
-  hangAngle:number;
+  swingAngle:number;
+
 }
 
-const bodyScale = 0.8;
+interface ILiftContainer {
+  liftHeight: number
+}
+
+const bodyScale = 0.65;
+const swingTime = 3;
+
+export const LiftContainer = styled.div<ILiftContainer>`
+
+  transform:translateY(-${(props) => props.liftHeight}px) scale(${bodyScale});
+  transition: transform 2s ;
+`;
 
 export const Container = styled.div<IBodyContainer>`
-  top: 800px;
+  top: 500px;
+  left: -200px;
 
   position:relative;
-  transform: scale(${bodyScale});
-
-  &:not(.sit) {
-    //transform:translateY(100px);
-    
+  
+  img.ropeImg {
+    left: 628px;
+    top: -423px;
   }
 
   &.swing{
-
-   
-    animation: swinging 2.5s ease-in-out infinite;
+    animation: swinging ${swingTime}s ease-in-out infinite;
     
-    transform-origin: center -1000px
+    animation-delay: -${swingTime / 2}s;
+    transform: rotate(0) ;
+    transform-origin: center -400px;
+
+    transition: transform ${swingTime}s ;
   }
 
+  @keyframes swing2 {
+    0% {
+      transform: rotate(0) ;
+    }
+    100% {
+      transform:  rotate(${(props) => props.swingAngle}deg);
+    }
+  }
   @keyframes swinging {
-  
-  0% , 100%  {transform: translateX(-0px) rotate(${(props) => props.hangAngle}deg) scale(${bodyScale});}
-  
-  50% {transform: translateX(0px) rotate(-${(props) => props.hangAngle}deg) scale(${bodyScale});}
- 
-}
-  
+    0% , 100%  {
+      transform: rotate(${(props) => props.swingAngle}deg) ;
+    }
+    50% {
+      transform:  rotate(-${(props) => props.swingAngle}deg);
+    }
+  }
 
-  //transition: transform 0.5s;
 
 `;
