d5bcd8abec0a7d8db8f9b5a9b6b02cc31f4625e7
kortkamp <marcelusmedius@gmail.com>
2021-10-17 14:26:29 -0300

    feat: test penetrable tool

--- src/pages/TrainingBed/index.tsx
+++ src/pages/TrainingBed/index.tsx
@@ -7,21 +7,24 @@ import { useMousePosition } from '../../hooks/useMousePosition';
 import StatusBox from '../../components/StatusBox';
 
 import backgroundImg from '../../assets/bgimage/H/Hx1_0.jpg';
+import PenetratingTool from '../../components/PenetratingTool';
 // import { useSlave } from '../../hooks/useSlave';
 
 // import testImg from '../../assets/fgimage/Hx1/tools/plug.png';
 
 const MainRoom = ():JSX.Element => {
   // eslint-disable-next-line no-unused-vars
-  // const position = useMousePosition();
+  const { mousePosition } = useMousePosition();
   console.log('main room redraw');
   // const { status } = useSlave();
+
   return (
     <Container>
 
       <img className="background" src={backgroundImg} alt="" />
 
       <Body />
+      <PenetratingTool initialPosition={{ x: 753, y: 647 }} limit={250} />
       {/* <img src={testImg} alt="" /> */}
       {/* <div>
         {position.x}
@@ -32,6 +35,7 @@ const MainRoom = ():JSX.Element => {
         {status.lust}
       </div> */}
       <StatusBox />
+      <span>{`x:${mousePosition.x} y:${mousePosition.y}`}</span>
     </Container>
   );
 };
--- src/pages/TrainingBed/styles.ts
+++ src/pages/TrainingBed/styles.ts
@@ -6,6 +6,9 @@ export const Container = styled.div`
   position: relative;
   width:fit-content;
 
+  width: 1350px;
+  height: 900px;
+
   img {
     top:0;
     left:0;
