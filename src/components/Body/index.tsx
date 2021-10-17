9f568403c080475ec58cdd2d0330fcb1a2346c2a
kortkamp <marcelusmedius@gmail.com>
2021-10-17 14:27:05 -0300

    feat: use pentrating tool

--- src/components/Body/index.tsx
+++ src/components/Body/index.tsx
@@ -2,11 +2,16 @@ import { Container } from './styles';
 import Head from '../Head';
 import { EmotionProvider } from '../../hooks/useEmotion';
 
+// import bodyImg from '../../assets/fgimage/Hx1/body/body0.png';
+import Ass from '../Ass';
+
 // eslint-disable-next-line no-unused-vars
 const Body = ():JSX.Element => (
   <Container>
     <EmotionProvider>
+      {/* <img src={bodyImg} alt="" /> */}
       <Head />
+      <Ass />
     </EmotionProvider>
   </Container>
 );
