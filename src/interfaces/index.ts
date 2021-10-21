159a3e86c2c4b5b9c87f848aaea129aa1d373d91
kortkamp <marcelusmedius@gmail.com>
2021-10-21 10:45:09 -0300

    feat: put interfaces in specific file

---
+++ src/interfaces/index.ts
@@ -0,0 +1,14 @@
+interface ISlaveStatus {
+  lust:number;
+  pain:number;
+  fear:number;
+  energy:number;
+  oxygen:number;
+  health: number;
+  ass:{
+    stretch: number,
+    depth: number,
+  }
+}
+
+export default ISlaveStatus;
