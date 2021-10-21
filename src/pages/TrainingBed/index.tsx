2b0ef8e53a3320fc51116bcf1cbb081cedf77bd5
kortkamp <marcelusmedius@gmail.com>
2021-10-21 15:33:06 -0300

    style: clean file

--- src/pages/TrainingBed/index.tsx
+++ src/pages/TrainingBed/index.tsx
@@ -1,44 +1,46 @@
 import { useHistory } from 'react-router-dom';
+import { useState } from 'react';
 import { Container } from './styles';
 import Body from '../../components/Body';
 
-// eslint-disable-next-line no-unused-vars
-import { useMousePosition } from '../../hooks/useMousePosition';
+import { IToolData } from '../../tools';
 
 import StatusBox from '../../components/StatusBox';
 
 import backgroundImg from '../../assets/bgimage/H/Hx1_0.jpg';
 import PenetratingTool from '../../components/PenetratingTool';
-// import { useSlave } from '../../hooks/useSlave';
 
-// import testImg from '../../assets/fgimage/Hx1/tools/plug.png';
+import ToolsBox from '../../components/ToolsBox';
+import ElasticTool from '../../components/ElasticTool';
+// import { useSlave } from '../../hooks/useSlave';
 
 const MainRoom = ():JSX.Element => {
   // eslint-disable-next-line no-unused-vars
   // const { mousePosition } = useMousePosition();
-  console.log('main room redraw');
+
   // const { status } = useSlave();
   const history = useHistory();
+  // const { hurt } = useSlave();
 
+  const [tool, setTool] = useState<IToolData>();
+  console.log('oh my god rendered room');
   return (
     <Container>
 
       <img className="background" src={backgroundImg} alt="" />
 
       <Body />
-      <PenetratingTool initialPosition={{ x: 753, y: 647 }} limit={111} />
-      {/* <img src={testImg} alt="" /> */}
-      {/* <div>
-        {position.x}
-        :
-        {position.y}
-        --
-        {' '}
-        {status.lust}
-      </div> */}
+      {tool?.type === 'penetrator'
+      && <PenetratingTool initialPosition={{ x: 753, y: 647 }} tool={tool} />}
+
+      {tool?.type === 'elastic'
+      && <ElasticTool initialPosition={{ x: 520, y: 350 }} tool={tool} action={() => {}} />}
+
       <StatusBox />
-      {/* <span>{`x:${mousePosition.x} y:${mousePosition.y}`}</span> */}
       <button type="button" onClick={() => history.push('/hang')}>Goto Hang</button>
+      <button type="button">Tools</button>
+
+      <ToolsBox selectTool={(selectedTool) => setTool(selectedTool)} />
     </Container>
   );
 };
