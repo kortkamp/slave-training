6f611772a691ff85534c40c81e5c60243a7719af
kortkamp <marcelusmedius@gmail.com>
2021-10-23 11:08:49 -0300

    feat: add bulge effect

--- src/components/Ass/index.tsx
+++ src/components/Ass/index.tsx
@@ -3,6 +3,8 @@ import { Container } from './styles';
 import overAssStretch from '../../assets/fgimage/Hx1/body/ass_stretch_6o.png';
 import behindAssStretch from '../../assets/fgimage/Hx1/body/ass_stretch_6b.png';
 import overBelly from '../../assets/fgimage/Hx1/body/over_ass.png';
+import bulge1 from '../../assets/fgimage/Hx1/body/bulge1.png';
+import bulge2 from '../../assets/fgimage/Hx1/body/bulge2.png';
 import { useSlave } from '../../hooks/useSlave';
 
 // eslint-disable-next-line no-unused-vars
@@ -15,6 +17,10 @@ const Ass = ():JSX.Element => {
       <img src={overBelly} alt="" style={{ zIndex: 1009 }} />
       <img src={behindAssStretch} alt="" style={{ zIndex: 1000, transformOrigin: '753px 647px', transform: `scale(${stretchScale})` }} />
       <img src={overAssStretch} alt="" style={{ zIndex: 1010, transformOrigin: '753px 647px', transform: `scale(${stretchScale})` }} />
+      {status.ass.depth > 200
+      && <img src={bulge1} alt="" style={{ zIndex: 1010 }} />}
+      {status.ass.depth > 220
+      && <img src={bulge2} alt="" style={{ zIndex: 1011 }} />}
     </Container>
   );
 };
