c02fdd6f345e2ce34708973b22de1b2f24d1378f
kortkamp <marcelusmedius@gmail.com>
2021-10-25 13:02:27 -0300

    refactor: change context order

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import Modal from 'react-modal';
 
 import { SlaveProvider } from './hooks/useSlave';
 import { useWindowDimensions } from './hooks/useWindowDimensions';
 import { GlobalStyle } from './styles/global';
 
 import Routes from './routes';
+import { EmotionProvider } from './hooks/useEmotion';
+import { AssProvider } from './hooks/useAss';
+
+Modal.setAppElement('#root');
 
 function App() {
   const { scale } = useWindowDimensions();
   return (
     <BrowserRouter>
-      <SlaveProvider>
-        <Routes />
-      </SlaveProvider>
+      <EmotionProvider>
+        <AssProvider>
+          <SlaveProvider>
+            <Routes />
+          </SlaveProvider>
+        </AssProvider>
+      </EmotionProvider>
       <GlobalStyle scale={scale} />
     </BrowserRouter>
   );
