3c3c57a26b485913b04a237c07c591149620c33d
kortkamp <marcelusmedius@gmail.com>
2021-10-27 08:10:39 -0300

    feat: add animated head

--- src/pages/Bedroom/index.tsx
+++ src/pages/Bedroom/index.tsx
@@ -1,4 +1,7 @@
+/* eslint-disable no-unused-vars */
 import { Container } from './styles';
+
+import Head from '../../components/Head';
 
 import StatusBox from '../../components/StatusBox';
 
@@ -13,20 +16,33 @@ import eyeImg from '../../assets/fgimage/s/face/F/e_def.gif';
 import mouthImg from '../../assets/fgimage/s/face/F/m_def.png';
 
 import hairImg from '../../assets/fgimage/s/body/stn_fh.png';
+import { useSlave } from '../../hooks/useSlave';
+import ToolsBox from '../../components/ToolsBox';
 
-const Bedroom = ():JSX.Element => (
-  <Container>
+const Bedroom = ():JSX.Element => {
+  const { sleep, load, status } = useSlave();
+  return (
+    <Container>
 
-    <img className="background" src={backgroundImg} alt="" />
-    <img src={bodyImg} alt="" />
-    <img src={headImg} alt="" />
-    <img src={eyeImg} alt="" />
-    <img src={mouthImg} alt="" />
-    <img src={hairImg} alt="" />
-    <img src={dressImg} alt="" />
-    <img src={armsImg} alt="" />
+      <img className="background" src={backgroundImg} alt="" />
+      <img src={bodyImg} alt="" />
 
-    <StatusBox />
-  </Container>
-);
+      {/* <img src={headImg} alt="" />
+      <img src={eyeImg} alt="" />
+      <img src={mouthImg} alt="" /> */}
+      <div className="headContainer">
+        <Head />
+      </div>
+
+      <img src={hairImg} alt="" />
+      <img src={dressImg} alt="" />
+      <img src={armsImg} alt="" />
+
+      <button type="button" onClick={() => sleep()}>SLEEP</button>
+      <button type="button" onClick={() => load()}>LOAD</button>
+      <StatusBox />
+      <ToolsBox />
+    </Container>
+  );
+};
 export default Bedroom;
--- src/pages/Bedroom/styles.ts
+++ src/pages/Bedroom/styles.ts
@@ -6,6 +6,16 @@ export const Container = styled.div`
   position: relative;
   width:fit-content;
 
+  div.headContainer {
+    transform: rotate(42deg) scale(0.85);
+    /* opacity:0.3; */
+    position: absolute;
+    top: -60px;
+    left: 554px; 
+    /* transform-origin: '0px 0px'; */
+    /* transform: 'scale(0.8)'; */
+  }
+
   img {
     top:0;
     left:0;
