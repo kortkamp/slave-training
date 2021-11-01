680ef1fb31c12d9d1939fcb9077289d4e57d1f42
kortkamp <marcelusmedius@gmail.com>
2021-11-01 15:47:22 -0300

    feat: use reducer hook in chatbox

--- src/components/ChatBox/index.tsx
+++ src/components/ChatBox/index.tsx
@@ -1,48 +1,90 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
+import clone from 'clone-deep';
 import { Container, Option } from './styles';
 
 interface IChatOption {
   text: string;
-  action: () => void;
+  action?: () => void;
   next?: IChatSequence|undefined;
   reset?: boolean;
+  back?:number;
   finish?:boolean;
 }
 export interface IChatSequence {
   // eslint-disable-next-line react/no-unused-prop-types
   text: string;
   // eslint-disable-next-line react/no-unused-prop-types
-  options: IChatOption[];
+  options?: IChatOption[];
+}
+interface IReducerStateAction {
+  type: 'clear'|'go'|'back';
+  ammount?: number;
+  state?: IChatSequence;
+}
+function chatReducer(state:IChatSequence[], action:IReducerStateAction) {
+  switch (action.type) {
+    case 'go':
+      if (action.state) {
+        state.unshift(clone(action.state));
+        return state;
+      }
+      return state;
+    case 'back':
+      if (action.ammount) {
+        console.log(action.ammount);
+        state.splice(0, action.ammount);
+        return state;
+      }
+      state.shift();
+      return state;
+    case 'clear':
+      return [];
+    default:
+      throw new Error();
+  }
 }
 
 const ChatBox = ({ initchat }:{initchat:IChatSequence}):JSX.Element => {
-  const [chat, setChat] = useState<IChatSequence|undefined>(initchat);
+  const [chatStack, dispachChat] = useReducer(chatReducer, [initchat]);
 
   return (
     <Container>
 
-      <div className="text">
-        { chat?.text || '...'}
+      <div key={chatStack[0]?.text} className="text">
+        { chatStack[0]?.text || '...'}
       </div>
-      <div className="options">
-        {chat?.options.map((option) => (
+      <div key={`${chatStack[0].text}-`} className="options">
+        {chatStack[0].options?.map((option) => (
           <Option
+            key={option.text}
             onClick={() => {
-              option.action();
-              if (option.next) {
-                setChat(option.next);
+              if (option.action) {
+                option.action();
               }
-              if (option.reset) {
-                setChat(initchat);
+              if (option.next) {
+                dispachChat({ type: 'go', state: option.next });
+              }
+              if (option.back) {
+                dispachChat({ type: 'back', ammount: option.back });
               }
               if (option.finish) {
-                setChat(undefined);
+                dispachChat({ type: 'clear' });
+              }
+              if (option.reset) {
+                dispachChat({ type: 'clear' });
+                dispachChat({ type: 'go', state: initchat });
               }
             }}
           >
             {option.text}
           </Option>
         ))}
+        {chatStack.length > 1
+        && (
+          <Option onClick={() => { dispachChat({ type: 'back' }); }}>
+            Back
+          </Option>
+        )}
       </div>
 
     </Container>
